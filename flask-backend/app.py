c2b1655b53eb120e3214e82dd8c4db52f942afc8
putraasmarjoe <putraasmarjoe@gmail.com>
2024-07-13 02:02:03 +0800

    update readme

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from config import Config
